7f49243b5974c81257f877aa4ef91128728be9b5
letuslogis-lgtm <letus.logis@gmail.com>
2026-04-10 16:06:46 +0900

    Update WorkerManagement.jsx

--- WorkerManagement.jsx
+++ WorkerManagement.jsx
@@ -1,5 +1,7 @@
 const { useState, useEffect, useMemo } = React;
 
+const supabaseClient = window.supabase;
+
 // ✖️ 공통 닫기 아이콘
 const CloseIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
